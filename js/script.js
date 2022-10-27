f41b319a3dc7d9f205f7aa58a125d490dd1e5c36
Kazybek <seidakhmetov.kazybek@gmail.com>
2022-10-27 14:48:46 +0600

    Практика, ч.4

--- js/script.js
+++ js/script.js
@@ -1,64 +1,78 @@
 "use strict";
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-
-start();
-
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {    
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                  b = prompt('На сколько оцените его?', '');
+            if (a == '' || a == null || a.length > 50 || b == '' || b == null || b.length > 50) {
+                i--;
+            } else {
+                personalMovieDB.movies[a] = b;
+            }    
+        }
+    },
+    detectPersonalLavel: function() {
+        if (personalMovieDB.count <= 10) {
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count <= 30) {
+            console.log("Вы классический зритель");
+        } else if (personalMovieDB.count > 30) {
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 0; i < 3; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i+1}`);
+
+            if (genre === '' || genre == null) {
+                console.log('Вы ввели некорректные данные или не ввели их вовсе');
+                i--;
+            } else {
+                personalMovieDB.genres[i] = genre;
+            } 
+        }
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
+    },
+    showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    toggleVisidleMyDB: function () {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    }
+    
 };
 
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {    
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его?', '');
-        if (a == '' || a == null || a.length > 50 || b == '' || b == null || b.length > 50) {
-            i--;
-        } else {
-            personalMovieDB.movies[a] = b;
-        }    
-    }
-}
+personalMovieDB.start();
 
-rememberMyFilms();
+personalMovieDB.rememberMyFilms();
 
-function detectPersonalLavel() {
-    if (personalMovieDB.count <= 10) {
-        console.log("Просмотрено довольно мало фильмов");
-    } else if (personalMovieDB.count <= 30) {
-        console.log("Вы классический зритель");
-    } else if (personalMovieDB.count > 30) {
-        console.log("Вы киноман");
-    } else {
-        console.log("Произошла ошибка");
-    }
-}
+personalMovieDB.detectPersonalLavel();
 
-detectPersonalLavel();
+personalMovieDB.writeYourGenres();
 
-function writeYourGenres() {
-    for (let i = 0; i < 3; i++) {
-        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`, '');
-    }
-}
+personalMovieDB.showMyDB(personalMovieDB.privat);
 
-writeYourGenres();
-
-function showMyDB (hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-
-showMyDB(personalMovieDB.privat);
+personalMovieDB.toggleVisidleMyDB();
